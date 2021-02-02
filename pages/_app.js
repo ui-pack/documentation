5779f9bcc9092cc9b63eefa51b798f4c4eb56403
Joseph Rex <joerexko@gmail.com>
2021-02-01 22:18:27 -0600

    Add mdx support and set up syntax highlighting

--- pages/_app.js
+++ pages/_app.js
@@ -1,11 +1,17 @@
+import { MDXProvider } from '@mdx-js/react'
 import GlobalStyle from '../styles/globals'
+import code from '../components/codeblock'
+
+const components = {
+  code
+}
 
 function MyApp({ Component, pageProps }) {
   return (
-    <>
+    <MDXProvider components={components}>
       <GlobalStyle />
       <Component {...pageProps} />
-    </>
+    </MDXProvider>
   )
 }
 
