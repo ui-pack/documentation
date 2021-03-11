c3b2775054f7474604ce64b65befe0e5590e4f48
Joseph Rex <joerexko@gmail.com>
2021-03-10 20:19:13 -0600

    Add content to installation section

--- components/codeblock.js
+++ components/codeblock.js
@@ -1,11 +1,24 @@
+import * as React from 'react'
 import Highlight, { defaultProps } from 'prism-react-renderer'
 import theme from "prism-react-renderer/themes/nightOwl";
 import { LiveProvider, LiveEditor, LiveError, LivePreview } from 'react-live'
 import { mdx } from '@mdx-js/react'
-import styled from 'styled-components'
-// import * as fs from 'fs';
-// import { transformSync } from '@babel/core'
+import styled, { css } from 'styled-components'
 import Masonry from '@ui-pack/react/masonry'
+
+const Tab = styled.button`
+  padding: 1.2rem 2rem;
+  border: 0;
+  background: none;
+  color: hsl(var(--color-gray-50));
+  font: var(--base-font);
+  font-size: 1.4rem;
+  outline: none;
+  cursor: pointer;
+  ${({ active }) => active && css`
+    background: rgb(1 22 39);
+  `}
+`
 
 const Image = styled.img`
   display: block;
@@ -19,8 +32,55 @@ const scope = {
   Image
 }
 
-export default function code({children, className = '', live, render}) {
+export default function code({children, className = '', live, render, tabs}) {
   const language = className.replace(/language-/, '')
+  const [activeIndex, setActiveIndex] = React.useState(0)
+
+  if (tabs) {
+    const tabTriggers = tabs.replace(/('|")/g, '').split('|')
+    const tabPanels = children.split('--')
+    return (
+      <Highlight {...defaultProps} code={tabPanels[activeIndex].trim()} language={language} theme={theme}>
+        {({className, style, tokens, getLineProps, getTokenProps}) => (
+          <div style={{ background: 'rgb(1 22 39)', borderRadius: 'var(--base-curve)', overflow: 'hidden' }} role="tablist">
+            <div style={{ background: 'rgb(255 255 255 / .03)' }}>
+            {
+              tabTriggers.map((trigger, key) => (
+                <Tab
+                  key={trigger}
+                  active={activeIndex === key}
+                  aria-selected={activeIndex === key}
+                  onClick={() => setActiveIndex(key)}
+                  role="tab"
+                >
+                {trigger}
+                </Tab>
+              ))
+            }
+            </div>
+          <pre
+            className={className}
+            role="tabpanel"
+            style={{
+              ...style,
+              padding: '20px',
+              overflow: 'auto',
+              fontSize: '1.6rem'
+            }}
+          >
+            {tokens.map((line, i) => (
+              <div key={i} {...getLineProps({line, key: i})}>
+                {line.map((token, key) => (
+                  <span key={key} {...getTokenProps({token, key})} />
+                ))}
+              </div>
+            ))}
+          </pre>
+          </div>
+        )}
+      </Highlight>
+    )
+  }
 
   if (live) {
     return (
@@ -29,15 +89,7 @@ export default function code({children, className = '', live, render}) {
           code={children.trim()}
           theme={theme}
           disabled
-          transformCode={code => {
-            // const transformed = transformSync(code, {
-            //   plugins: [
-            //     require('@babel/plugin-syntax-jsx')
-            //   ]
-            // }).code
-            // return transformed
-            return code
-          }}
+          transformCode={code => code}
           scope={scope}
         >
           <LivePreview
