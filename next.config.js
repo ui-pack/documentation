80ba41aaddc5e92877594c9d91e2e911cec4dbbb
Joseph Rex <joerexko@gmail.com>
2021-03-20 23:10:41 -0500

    Switch to using frontmatter with components

--- next.config.js
+++ next.config.js
@@ -1,5 +1,11 @@
+const frontmatterPlugin = require('./lib/frontmatter')
+
+
 const withMDX = require('@next/mdx')({
   extension: /\.mdx?$/,
+  options: {
+    remarkPlugins: [frontmatterPlugin]
+  }
 })
 module.exports = withMDX({
   pageExtensions: ['js', 'jsx', 'md', 'mdx'],
